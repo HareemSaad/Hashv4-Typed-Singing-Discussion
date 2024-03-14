357a2a4ccdd45cae10ef5cd6d51d00cf8bff74b5
HareemSaad <hareems20014@outlook.com>
2024-03-14 16:18:44 +0500

    script setup

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import "src/Signing.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(vm.envUint("PK"));
+        new Signer();
     }
 }
